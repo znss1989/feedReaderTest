822b76308df9cc3aac2b028976ff7c0a00ebe884
znss1989 <znss1989@gmail.com>
2015-12-20 20:15:44 +0800

    To fail the initail test

--- js/app.js
+++ js/app.js
@@ -22,6 +22,7 @@ var allFeeds = [
         url: 'http://feeds.feedburner.com/udacity-linear-digressions'
     }
 ];
+allFeeds = [];
 
 /* This function starts up our application. The Google Feed
  * Reader API is loaded asynchonously and will then call this
